7715a70aaf2e85ff9c89a0804c6ca0383fd18fd0
Colt <Colt@Colts-MacBook-Pro.local>
2015-08-19 10:28:41 -0700

    asdas

--- js/app.js
+++ js/app.js
@@ -1,1 +1,1 @@
-var app = angular.module("myApp", ['firebase'])
+var app = angular.module("myApp", ['firebase', 'chart.js'])
--- js/controllers.js
+++ js/controllers.js
@@ -1,16 +1,35 @@
-app.controller('TestController', function($scope, $firebaseArray){
+app.controller('TestController', function($scope, $firebaseObject){
 	var root = new Firebase("https://gschooldemo.firebaseio.com");
-	var placesRef = new Firebase("https://gschooldemo.firebaseio.com/places");
+	var scoresRef = new Firebase("https://gschooldemo.firebaseio.com/test/-Jx3_ObiBa1KhP9FgfSG/data");
 
-	$scope.places = $firebaseArray(placesRef);
+	$scope.labels = ["One", "Two", "Three", "Four", "Five"];
+  $scope.data = [1,4,10,10,1];
+
+	$scope.responses = $firebaseObject(scoresRef);
+
+	// $scope.responses.$add({2:5})
+	// $scope.responses.$add({3:10})
+	// $scope.responses.$add({4:11})
+	// $scope.responses.$add({
+	// 	1:4,
+	// 	2:5,
+	// 	3:10,
+	// 	4:11,
+	// 	5:5
+	// })
+
+	// $scope.responses.$add({data: [2,4,10,10]})
+	// $scope.responses.$add(newMsg);
 
 	$scope.addPlace = function(){
-		var newPlace = {
-			name: $scope.newPlace
-		}
 
-		$scope.places.$add(newMsg);
-		$scope.newPlace = "";
+		$scope.data = [10,10,10,10,10];
+		// var newPlace = {
+		// 	name: $scope.newPlace
+		// }
+
+		// $scope.places.$add(newMsg);
+		// $scope.newPlace = "";
 	}
 
 	$scope.remove = function(msg){
@@ -18,7 +37,5 @@ app.controller('TestController', function($scope, $firebaseArray){
 	}
 
 });
-
-
 
 
