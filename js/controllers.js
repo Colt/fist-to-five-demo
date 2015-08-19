fd01f15912b6aea293866c12c68ef5f8b40882f8
Colt <Colt@Colts-MacBook-Pro.local>
2015-08-19 11:55:07 -0700

    clean up code

--- js/controllers.js
+++ js/controllers.js
@@ -1,16 +1,7 @@
-app.controller('TestController', function($scope, $firebaseArray){
-	var root = new Firebase("https://gschooldemo.firebaseio.com");
+app.controller('VotesController', function($scope, $firebaseArray){
 	var scoresRef = new Firebase("https://gschooldemo.firebaseio.com/test/-Jx3_ObiBa1KhP9FgfSG/data");
-		Chart.defaults.global.colours = ["#e74c3c", "#e67e22", "#f1c40f", "#2ecc71", "#27ae60","#c0392b", "#c0392b"]
-
-		
-
-	console.log(Chart.defaults.global.colours)
-
+	Chart.defaults.global.colours = ["#e74c3c", "#e67e22", "#f1c40f", "#2ecc71", "#27ae60","#c0392b", "#c0392b"]
 	$scope.labels = ["One", "Two", "Three", "Four", "Five"];
-  $scope.data = [1,1,1,1,1];
-  $scope.voted = false;
-
 	$scope.responses = $firebaseArray(scoresRef)
 
 	$scope.responses.$loaded().then(function(data){
@@ -23,25 +14,8 @@ app.controller('TestController', function($scope, $firebaseArray){
   	$scope.data[i] = newVal;
 	});
 
-	// $scope.test = $scope.responses.map(function(data){
-	// 	return data.$value
-	// })
-
-	// $scope.responses.$add({2:5})
-	// $scope.responses.$add({3:10})
-	// $scope.responses.$add({4:11})
-	// $scope.responses.$add({
-	// 	1:4,
-	// 	2:5,
-	// 	3:10,
-	// 	4:11,
-	// 	5:5
-	// })
-
-	// $scope.responses.$add({data: [2,4,10,10]})
-	// $scope.responses.$add(newMsg);
-
 	$scope.reset = function(){
+		//obviously need to refactor this
 		$scope.responses[0].$value = 1;
 		$scope.responses[1].$value = 1;
 		$scope.responses[2].$value = 1;
@@ -52,21 +26,13 @@ app.controller('TestController', function($scope, $firebaseArray){
 		$scope.responses.$save(2)
 		$scope.responses.$save(3)
 		$scope.responses.$save(4)
-
-		// then(function(){
-		// 	$scope.data = $scope.responses.map(function(x){return x.$value});
-		// })
 	}
 
-	$scope.addPlace = function(vote){
+	$scope.vote = function(vote){
 		$scope.responses[vote].$value += 1;
 		$scope.responses.$save(vote).then(function(){
 			$scope.data = $scope.responses.map(function(x){return x.$value});
 		})
-	}
-
-	$scope.remove = function(msg){
-		$scope.messages.$remove(msg);
 	}
 
 });
